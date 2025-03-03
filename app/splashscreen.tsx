5b06f738aab3015e2fbe565572494aa403627952
10santiago12 <surrego1303@gmail.com>
2025-03-02 23:06:04 -0500

    splash complete

--- app/splashscreen.tsx
+++ app/splashscreen.tsx
@@ -1,15 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View,StyleSheet} from 'react-native';
+import { Image } from 'expo-image';
+import React from 'react';
 
 export default function Page() {
   return (
-    <View
-    style={{
-        backgroundColor: "gray",
-    }}
-    >
-
+    <View style={styles.container}>
+      <Image
+        source="https://freelogopng.com/images/all_img/1681039182chatgpt-logo-with-name.png"
+        style={styles.logo}
+      />
     </View>
-  )
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1E1E1E",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 500,
+    height: 500,
+    resizeMode: "contain",
+  },
+});
